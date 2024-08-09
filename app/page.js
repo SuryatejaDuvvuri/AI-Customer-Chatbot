b750cede2898a297acc04739b5bf807d6c5deecd
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2024-08-09 23:28:56 +0530

    Edited files

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 'use client'
 import { useState } from 'react';
-import { OpenAI } from "@langchain/openai";
+import { OpenAI } from "openai";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default function Home() {
 
@@ -13,20 +16,36 @@ export default function Home() {
     },
   ])
   const sendMessage = async () => {
-    const llm = new OpenAI({ modelName: "gpt-3.5-turbo" });
-    await llm.invoke("Hello, world!");
+
+    
+    const openai = new OpenAI({
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: process.env.OPENAI_API_KEY,
+    })
+    // await llm.invoke("Hello, world!");
+    const completion = await openai.chat.completions.create({
+      model: "openai/gpt-3.5-turbo",
+      messages: [
+      {
+        role: 'user',
+        content: "Hi! How are you doing?",
+      }
+      ],
+    })
+
+    console.log(completion.choices[0].message)
   }
   return (
-  <div class="w-screen h-screen flex flex-col justify-center items-center">
-  <div class="flex flex-col w-[500px] h-[700px] border border-white p-2 space-y-3">
-    <div class="flex flex-col space-y-2 flex-grow overflow-auto max-h-full">
+  <div className="w-screen h-screen flex flex-col justify-center items-center">
+  <div className="flex flex-col w-[500px] h-[700px] border border-white p-2 space-y-3">
+    <div className="flex flex-col space-y-2 flex-grow overflow-auto max-h-full">
       {messages.map((message, index) => (
         <div
           key={index}
-          class={`flex ${message.role === 'assistant' ? 'justify-start' : 'justify-end'}`}
+          className={`flex ${message.role === 'assistant' ? 'justify-start' : 'justify-end'}`}
         >
           <div
-            class={`${
+            className={`${
               message.role === 'assistant' ? 'bg-primary-main' : 'bg-secondary-main'
             } text-white rounded-xl p-3`}
           >
@@ -35,16 +54,16 @@ export default function Home() {
         </div>
       ))}
     </div>
-    <div class="flex flex-row space-x-2">
+    <div className="flex flex-row space-x-2">
       <input
         type="text"
-        color = "black"
         placeholder="Message"
-        class="w-full"
+        color='black'
+        className="w-full border border-white text-black rounded-xl p-3" // make the text black
         value={message}
         onChange={(e) => setMessage(e.target.value)}
       />
-      <button class="btn btn-contained" onClick={sendMessage}>
+      <button className="btn btn-contained" onClick={sendMessage}>
         Send
       </button>
     </div>
